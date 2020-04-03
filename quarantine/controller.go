9b578a4dcd0d0d9aa7a55e4ccd363d499c127ff0
Sujata Regoti <regotiss@users.noreply.github.com>
2020-04-03 21:00:16 +0530

    Sujata : Add basic controller for police and quarantine

---
+++ quarantine/controller.go
@@ -0,0 +1,19 @@
+package quarantine
+
+import "github.com/gin-gonic/gin"
+
+type Controller interface {
+	SaveProfileDetails(ctx *gin.Context)
+}
+
+type controller struct {
+
+}
+
+func (c controller) SaveProfileDetails(ctx *gin.Context) {
+	panic("implement me")
+}
+
+func NewController() Controller {
+	return controller{}
+}
--- router.go
+++ router.go
@@ -1,13 +1,39 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"trackcoro/police"
+	"trackcoro/quarantine"
+)
 
 func InitializeRouter() *gin.Engine {
 	router := gin.Default()
-	router.GET("/healthz", func(c *gin.Context) {
+	addHealthCheckRoute(router)
+	addRoutesForQuarantine(router)
+	addRoutesForPolice(router)
+	return router
+}
+
+func addHealthCheckRoute(router *gin.Engine) {
+	router.GET("/api/healthz", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"status": "OK",
 		})
 	})
-	return router
+}
+
+func addRoutesForQuarantine(router *gin.Engine) {
+	controller := quarantine.NewController()
+	quarantineGroup := router.Group("/api/v1/quarantine")
+	{
+		quarantineGroup.GET("/save", controller.SaveProfileDetails)
+	}
+}
+
+func addRoutesForPolice(router *gin.Engine) {
+	controller := police.NewController()
+	quarantineGroup := router.Group("/api/v1/police")
+	{
+		quarantineGroup.GET("/save", controller.SaveProfileDetails)
+	}
 }
