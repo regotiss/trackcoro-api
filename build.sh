9821dd7f99a1290a3a46a2eeeddf6e735f938a9b
linonymous <iamlinonymous@gmail.com>
2020-04-05 09:40:48 +0530

    update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 LAMBDA_PATH="./cmd/lambda/"
 SERVER_PATH="./cmd/server/"
 
-cd $LAMBDA_PATH && set GOOS=linux && set CGO_ENABLED=0 && set GOARCH=amd64 && go build && cd ../..
+# GO ENV variables are must for lambda deployment
+cd $LAMBDA_PATH && set GOOS=linux && set CGO_ENABLED=0 && set GOARCH=amd64 && go build && cd ../../
 cd $SERVER_PATH && go build && cd ../..
