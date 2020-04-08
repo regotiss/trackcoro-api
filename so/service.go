e390e9857f78052a70b68f3cfdc52d3c75692164
Sujata Regoti <regotiss@users.noreply.github.com>
2020-04-08 09:48:56 +0530

    Sujata : Send quarantine current location details for SO

--- so/service.go
+++ so/service.go
@@ -1,6 +1,7 @@
 package so
 
 import (
+	"trackcoro/constants"
 	models2 "trackcoro/database/models"
 	"trackcoro/models"
 	"trackcoro/utils"
@@ -32,9 +33,20 @@ func (s service) GetQuarantines(soMobileNumber string) ([]models.QuarantineDetai
 	}
 	var quarantineDetails []models.QuarantineDetails
 	for _, quarantine := range quarantinesFromDB {
-		quarantineDetails = append(quarantineDetails, utils.GetMappedQuarantine(quarantine))
+		quarantineDetails = append(quarantineDetails, getQuarantineDetails(quarantine))
 	}
 	return quarantineDetails, nil
+}
+
+func getQuarantineDetails(quarantine models2.Quarantine) models.QuarantineDetails {
+	mappedQuarantine := utils.GetMappedQuarantine(quarantine)
+	if quarantine.CurrentLocationLatitude != constants.Empty || quarantine.CurrentLocationLongitude != constants.Empty {
+		mappedQuarantine.CurrentLocation = &models.Coordinates{
+			Latitude:  quarantine.CurrentLocationLatitude,
+			Longitude: quarantine.CurrentLocationLongitude,
+		}
+	}
+	return mappedQuarantine
 }
 
 func (s service) DeleteQuarantine(soMobileNumber string, quarantineMobileNumber string) error {
